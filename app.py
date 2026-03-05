8e3c233a4dd8de36c3eab4ae6cc09b4c0135e485
Roshini <roshiniammu63@gmail.com>
2026-03-05 13:00:02 +0530

    ROI box and prediction fix

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, jsonify
 import numpy as np
 import base64
 import cv2
-from keras.models import load_model
 import os
+from keras.models import load_model
 
 app = Flask(__name__)
 
+# Lazy model loading
 model = None
 
 def get_model():
@@ -23,30 +24,19 @@ labels = [
 ]
 
 
-# HOME PAGE
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-# LIVE CAMERA PAGE
 @app.route("/live")
 def live():
     return render_template("live.html")
 
 
-# ABOUT PAGE
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-# MODEL PREDICTION API
 @app.route("/predict", methods=["POST"])
 def predict():
-
     try:
-
         data = request.json["image"]
 
         encoded = data.split(",")[1]
@@ -55,19 +45,19 @@ def predict():
         np_arr = np.frombuffer(img_bytes, np.uint8)
         img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-        img = cv2.resize(img,(128,128))
-        img = img.astype("float32") / 255.0
-        img = np.expand_dims(img,axis=0)
+        img = cv2.resize(img, (128,128))
+        img = img / 255.0
+        img = np.expand_dims(img, axis=0)
 
-        prediction = get_model().predict(img, verbose=0)
-        label = labels[int(np.argmax(prediction))]
+        prediction = get_model().predict(img)
+
+        label = labels[np.argmax(prediction)]
 
         return jsonify({"prediction": label})
 
     except Exception as e:
-        print("Prediction error:", e)
-        return jsonify({"prediction": str(e)})
-
+        return jsonify({"prediction": "Error"})
+    
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",10000))
